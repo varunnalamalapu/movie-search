913893d615166a59cde0f83a80372d41cc0a2780
Nalamalapuvarun <varun.nalamalapu@gmail.com>
2020-08-27 16:52:19 -0400

    Updates

---
+++ precache-manifest.7e6cdb2536570bef08dfb75134094145.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3524e4a339f6d5d99c53c4b656068685",
+    "url": "/movie-search/index.html"
+  },
+  {
+    "revision": "7d248bba4c725871d5a5",
+    "url": "/movie-search/static/css/main.b5995665.chunk.css"
+  },
+  {
+    "revision": "d83114c55dfd20c7179b",
+    "url": "/movie-search/static/js/2.f7df0891.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-search/static/js/2.f7df0891.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d248bba4c725871d5a5",
+    "url": "/movie-search/static/js/main.24ab514e.chunk.js"
+  },
+  {
+    "revision": "fe6b5b1b4094f48aee8c",
+    "url": "/movie-search/static/js/runtime-main.fbec6586.js"
+  }
+]);
